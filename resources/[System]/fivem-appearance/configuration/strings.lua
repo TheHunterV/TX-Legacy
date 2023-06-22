7f4b0560f2b840c00dbc712b9d26415db09d4536
The Hunter V <44531252+TheHunterV@users.noreply.github.com>
2023-06-22 14:08:12 -0400

    Add files via upload
    Added System Files

---
+++ resources/[System]/fivem-appearance/configuration/strings.lua
@@ -0,0 +1,37 @@
+Strings = {
+    -- Text UI strings --
+    clothing_menu = '[E] - Changer de tenue',
+    barber_menu = '[E] - Changer de cheveux',
+    tattoo_menu = '[E] - Fait un tattoos',
+
+    -- Notifications --
+    success = 'Succes',
+    success_desc = 'Vous avez reussi a payer $%s pour un nouveau look!',
+
+    no_funds = 'Manquait dargent',
+    no_funds_desc = 'Vous n’avez pas assez de fonds a la banque pour cela',
+
+    -- Menus --
+
+    -- Main clothing shop menu
+    clothing_shop_title = 'Menu boutique',
+    change_clothing_title = 'Changer de tenue',
+    change_clothing_desc = 'Parcourir les vetements disponibles',
+    browse_outfits_title = 'Parcourir les tenues',
+    browse_outfits_desc = 'Parcourir les tenues enregistrees',
+    save_outfit_title = 'Enregistrer une tenue',
+    save_outfit_info = 'Nom de la tenue',
+    save_outfit_desc = 'Conserver la tenue actuelle',
+    delete_outfit_title = 'Supprimer les tenues',
+    delete_outfit_desc = 'Supprimer une tenue enregistree',
+
+    -- Browse/Delete outfits & Wardrobe
+    wardrobe_title = 'Garde-robe',
+    delete_outfits_title = 'Supprimer les tenues',
+    go_back_title = '< Retour',
+    go_back_desc = 'Pas de tenues sauvees!',
+
+    -- Other
+    skin_command_help = 'Changer de skin',
+    skin_command_arg_help = 'Player ID'
+}
