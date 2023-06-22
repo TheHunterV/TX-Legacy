abf48eaeeac915c4cc4c228fc26f50a2453cca63
The Hunter V <44531252+TheHunterV@users.noreply.github.com>
2023-06-22 14:19:23 -0400

    Add files via upload

---
+++ resources/[ESX]/esx_dmvschool/html/questions_en.js
@@ -0,0 +1,95 @@
+var tableauQuestion = [
+	{
+		question: "Si vous roulez à 80 km/h et que vous approchez d'une zone résidentielle, que devez-vous faire ?",
+		propositionA: "Accélérer",
+		propositionB: "Maintenir votre vitesse, si vous ne dépassez pas d'autres véhicules",
+		propositionC: "Ralentir",
+		propositionD: "Maintenir votre vitesse",
+		reponse: "C"
+	},
+	
+
+	{
+		question: "Si vous tournez à droite à un feu de circulation, mais que vous voyez un piéton traverser, que devez-vous faire ?",
+		propositionA: "Vous passez devant le piéton",
+		propositionB: "Vous vérifiez qu'il n'y a pas d'autres véhicules autour",
+		propositionC: "Vous attendez que le piéton ait traversé",
+		propositionD: "Vous tirez sur le piéton et continuez à conduire",
+		reponse: "C"
+	},
+
+	{
+		question: "Sans indication préalable, quelle est la vitesse dans une zone résidentielle ?",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+	
+	{
+		question: "Avant chaque changement de voie, que devez-vous faire ?",
+		propositionA: "Vérifier vos rétroviseurs",
+		propositionB: "Vérifier vos angles morts",
+		propositionC: "Signaler vos intentions",
+		propositionD: "Tout ce qui précède",
+		reponse: "D"
+	},
+	
+
+	{
+		question: "À quel niveau d'alcoolémie est-on considéré comme conduisant en état d'ivresse ?",
+		propositionA: "0,05%",
+		propositionB: "0,18%",
+		propositionC: "0,08%",
+		propositionD: "0,06%",
+		reponse: "C"
+	},
+	
+	{
+		question: "Quand peut-on continuer à conduire à un feu de circulation ?",
+		propositionA: "Quand il est vert",
+		propositionB: "Quand il n'y a personne dans l'intersection",
+		propositionC: "Vous êtes dans une zone scolaire",
+		propositionD: "Quand il est vert et/ou rouge et que vous tournez à droite",
+		reponse: "D"
+	},
+	
+
+	{
+		question: "Un piéton a un signal de ne pas traverser, que faites-vous ?",
+		propositionA: "Vous les laissez passer",
+		propositionB: "Vous observez avant de continuer",
+		propositionC: "Vous faites un signe pour leur dire de traverser",
+		propositionD: "Vous continuez car votre feu de circulation est vert",
+		reponse: "D"
+	},
+	
+	{
+		question: "Qu'est-ce qui est autorisé lorsqu'on dépasse un autre véhicule ?",
+		propositionA: "Vous le suivez de près pour le dépasser plus rapidement",
+		propositionB: "Vous le dépassez sans quitter la chaussée",
+		propositionC: "Vous conduisez sur le côté opposé de la route pour le dépasser",
+		propositionD: "Vous dépassez la limite de vitesse pour les dépasser",
+		reponse: "C"
+	},
+	
+	{
+		question: "Vous conduisez sur une autoroute qui indique une vitesse maximale de 120 km/h. Mais la plupart des conducteurs roulent à 125 km/h, donc vous ne devez pas conduire plus vite que :",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+	
+	{
+		question: "Lorsqu'un autre véhicule vous dépasse, il est important de NE PAS :",
+		propositionA: "Ralentir",
+		propositionB: "Vérifier vos rétroviseurs",
+		propositionC: "Observer les autres conducteurs",
+		propositionD: "Augmenter votre vitesse",
+		reponse: "D"
+	},
+	
+]
