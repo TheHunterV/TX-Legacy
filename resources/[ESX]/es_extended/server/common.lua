3596cc39f11a37142a9e1e4d408dccd744a66635
The Hunter V <44531252+TheHunterV@users.noreply.github.com>
2023-06-23 03:00:23 -0400

    Modified getSharedObject Coding

--- resources/[ESX]/es_extended/server/common.lua
+++ resources/[ESX]/es_extended/server/common.lua
@@ -14,12 +14,11 @@ Core.playersByIdentifier = {}
 
 Core.vehicleTypesByModel = {}
 
-AddEventHandler("esx:getSharedObject", function()
-	local Invoke = GetInvokingResource()
-	print(("[^1ERROR^7] Resource ^5%s^7 Used the ^5getSharedObject^7 Event, this event ^1no longer exists!^7 Visit https://documentation.esx-framework.org/tutorials/tutorials-esx/sharedevent for how to fix!"):format(Invoke))
+AddEventHandler("esx:getSharedObject", function(cb)
+  cb(ESX)
 end)
 
-exports('getSharedObject', function()
+exports('getSharedObect', function()
   return ESX
 end)
 
